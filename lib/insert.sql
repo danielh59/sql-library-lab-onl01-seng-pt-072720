f38f47e164d63b6688754c39312d26c055effe3b
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-06 18:25:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES  (1, "Harry Potter", 1, 1), (2, "A Song of Ice and Fire", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "Fantasy Fiction"), (2, "Medieval");
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
-INSERT INTO authors (id, name) VALUES (1, "J. K. Rowling"), (2, "George R. R. Martin");
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Harry Potter and the Chamber of Secrets", 1998, 1), (2, "Harry Potter and the Prisoner of Azkaban", 1999, 1), (3, "Harry Potter and the Half-Blood Prince", 2005, 1), (4, "Game of Thrones", 1996, 2), (5, "A Clash of Kings", 1998, 12, (6, "A Storm of Swords", 2000, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Snape", "Always", "human", 1, 1), (2, "Hagrid", "Harry — yer a wizard.", "giant/human", 1, 1), (3, "Luna Lovegood", "Don't worry. You're just as sane as I am.", "human", 1, 1), (4, "Molly Weasley", "NOT MY DAUGHTER, YOU BITCH.", "human", 1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Tywin Lannister", "A lion doesn’t concern himself with the opinions of a sheep", "human", 2, 2), (6, "Tyrion Lanister", "A Lanister always pays is debts", "human", 2, 2), (7, "Daenerys Targaryen", "If I look back I am lost", "human", 2, 2), (8, "Eddard Stark", "Winter is coming", "human", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES (9, 2, 7), (10, 4, 6), (11, 4, 7), (12, 8, 6), (13, 3, 2), (14, 5, 7), (15, 4, 8), (16, 6, 7);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
